8feb9f2cf334389e4489f2410329e707d6b3a106
nigelwal <35769273+nigelwal@users.noreply.github.com>
2018-01-24 14:51:22 +0000

    version 4 weekdays
    3pm update

--- js/add-content.js
+++ js/add-content.js
@@ -1,13 +1,19 @@
 var today = new Date();
 var hourNow = today.getHours();
 var greeting;
+var day = today.getDay();
+var weekDay;
+
+if (day ==3){
+	weekDay = "Wednesday";
+}
 
 if (hourNow > 18) {
-		greeting = "Good evening! ";
+		greeting = "Good evening, its " + weekDay;
 	} else if (hourNow > 12) {
-		greeting = "Good afternoon!";
+		greeting = "Good afternoon Nigel! Happy " + weekDay;
 	} else if (hourNow > 0) {
-		greeting = "Good Morning!";
+		greeting = "Good Morning Nigel! Happy " + weekDay;
 	} else {
 		greeting = "Welcome!";
 	}
