fb74dcc4ff10a1667b2003c3932613fa170d970c
nigelwal <35769273+nigelwal@users.noreply.github.com>
2018-01-24 14:36:16 +0000

    Version 3
    Updated code working

--- js/add-content.js
+++ js/add-content.js
@@ -1,5 +1,5 @@
 var today = new Date();
-var hourNow = today.getHours(0);
+var hourNow = today.getHours();
 var greeting;
 
 if (hourNow > 18) {
@@ -12,4 +12,4 @@ if (hourNow > 18) {
 		greeting = "Welcome!";
 	}
 	
-	document.write("<h3>" + greeting + "</h3>");|
+document.write("<h3>" + greeting + "</h3>");
