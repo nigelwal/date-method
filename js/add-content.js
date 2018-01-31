19f7ba20b6551a06b8f2718d81ef15a8fa6b1aba
nigelwal <35769273+nigelwal@users.noreply.github.com>
2018-01-31 14:24:39 +0000

    31 jan 2018
    31 jan 2018 updated

--- js/add-content.js
+++ js/add-content.js
@@ -2,20 +2,24 @@ var today = new Date();
 var hourNow = today.getHours();
 var greeting;
 var day = today.getDay();
-var weekDay;
 
-if (day ==3){
-	weekDay = "Wednesday";
-}
+var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+console.log("The day index is " + day);
+
+console.log("The days of the week are (array) " + daysOfTheWeek);
 
 if (hourNow > 18) {
-		greeting = "Good evening, its " + weekDay;
+		greeting = "Good evening, its " + daysOfTheWeek [day];
 	} else if (hourNow > 12) {
-		greeting = "Good afternoon Nigel! Happy " + weekDay;
+		greeting = "Good afternoon Nigel! Happy " + daysOfTheWeek [day];
 	} else if (hourNow > 0) {
-		greeting = "Good Morning Nigel! Happy " + weekDay;
+		greeting = "Good Morning Nigel! Happy " + daysOfTheWeek [day];
 	} else {
 		greeting = "Welcome!";
 	}
 	
 document.write("<h3>" + greeting + "</h3>");
+
+
+console.log("The days of the week " + daysOfTheWeek);
---
+++ js/weekday.js
@@ -0,0 +1,7 @@
+// Create an array and assign it values.
+var weekday;
+weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+// Show the first item from the array.
+var el = document.getElementById("weekday");
+el.textContent = weekday[2];
